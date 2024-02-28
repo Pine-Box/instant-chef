11e441bfe4dbddf3e3ddc76c893d42b7cee06645
Dan Sora <dannysora@gmail.com>
2024-02-28 21:41:39 +0000

    some changes to the components

--- src/context.jsx
+++ src/context.jsx
@@ -3,9 +3,13 @@ import React, {useContext} from "react";
 const AppContext = React.createContext();
 
 const AppProvider = ({Children}) => {
-    return <App.AppContext.Provider value = 'hello'>
+    return <App.AppContext.Provider value = {{name:'dan', role:'student'}}>
         {children}
     </App.AppContext.Provider>
+}
+
+export const useGlobalContext = () => {
+    return useContext(AppContext)
 }
 
 export {AppContext, AppProvider};
