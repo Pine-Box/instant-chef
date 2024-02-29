ad51ba2a89f4e32dab67b741fb4b6b5e84afa7b3
Dan Sora <dannysora@gmail.com>
2024-02-29 21:33:07 +0000

    style app container, add recipecard displaying recipes component

--- src/components/pages/Recipes.jsx
+++ src/components/pages/Recipes.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import RecipeCard from "../RecipeCard";
 
 function Recipes() {
-    return 
+  return (
+    <>
+      <h1>Popular recipies</h1>
+      <div className="row">
+        <RecipeCard />
+      </div>
+    </>
+  );
 }
 
-export default Recipes
+export default Recipes;
