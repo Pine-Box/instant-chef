004366b3d6e362bdf21964683283671a82efe35a
Dan Sora <dannysora@gmail.com>
2024-03-01 20:03:13 +0000

    done some changes on country selector with help of TA

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,41 @@
-import React from "react";
-import HandleCountrySelect from "./CountrySelector";
+import React, { useState } from "react";
+// import HandleCountrySelect from "./CountrySelector";
 
 function Home() {
+  const [recipes, setRecipes] = useState([]);
+
+  const selectCountryAndGetData = async (event) => {
+    console.log(event.target.value);
+    const data = await fetch(
+      "www.themealdb.com/api/json/v1/1/filter.php?a=Canadian"
+    );
+    // setRecipes(data);
+    setRecipes([1, 2, 3]);
+  };
   return (
     <>
-      <HandleCountrySelect />
+      <div class="form-floating">
+        <select
+          class="form-select"
+          id="floatingSelect"
+          aria-label="Floating label select example"
+          onChange={selectCountryAndGetData}
+        >
+          <option selected>Open this select menu</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="Whatever">Three</option>
+        </select>
+      </div>
+      {/* recipe container */}
+      {recipes && (
+        <div>
+          {recipes.map((recipe) => {
+            // render a recipe component with recipe data in here
+            return <div>whatevs</div>;
+          })}
+        </div>
+      )}
     </>
   );
 }
