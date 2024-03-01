81f00c902b3599d32a2a3d679dd233e3e7522bd0
howarceus <howarceus@gmail.com>
2024-03-01 17:01:09 +0000

    added basic functionality of modal for the favourites section

--- src/components/favourites/index.jsx
+++ src/components/favourites/index.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import FavouriteCard from "../favouriteCard";
 import "./style.css";
+import Modal from "react-modal";
 
 const Favourites = ({ favourites, onRemove }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
   if (!favourites || !Array.isArray(favourites) || favourites.length === 0) {
     return (
       <div className="container" id="containerStyle">
@@ -16,18 +27,59 @@ const Favourites = ({ favourites, onRemove }) => {
     <div className="container" id="containerStyle">
       <h3>Favourites</h3>
       <div className="row" id="jumbotron">
-        {favourites.map((recipe) => (
+
+      {favourites.slice(0, 4).map((recipe) => (
           <FavouriteCard
             key={recipe.idMeal}
             recipe={recipe}
             onRemove={() => onRemove(recipe.idMeal)}
           />
         ))}
+        {favourites.length > 4 && (
+          <button className="btn btn-primary" onClick={openModal}>
+            View All
+          </button>
+        )}
+
+        {/* {favourites.map((recipe) => (
+          <FavouriteCard
+            key={recipe.idMeal}
+            recipe={recipe}
+            onRemove={() => onRemove(recipe.idMeal)}
+          />
+        ))} */}
         {/* <FavouriteCard/>
             <FavouriteCard/>
             <FavouriteCard/>
             <FavouriteCard/> */}
+
+
+
+
       </div>
+
+
+      {/* Modal for View All */}
+      <Modal
+        isOpen={showModal}
+        onRequestClose={closeModal}
+        contentLabel="View All Modal"
+      >
+        <div>
+          <h2>All Favourite Recipes</h2>
+          {favourites.map((recipe) => (
+            <FavouriteCard
+              key={recipe.idMeal}
+              recipe={recipe}
+              onRemove={() => onRemove(recipe.idMeal)}
+            />
+          ))}
+          <button className="btn btn-secondary" onClick={closeModal}>
+            Close
+          </button>
+        </div>
+      </Modal>
+
     </div>
   );
 };
