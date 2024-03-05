99a697461ece87f3f07c842aa85da6d11a90ea9d
Dan Sora <dannysora@gmail.com>
2024-03-05 21:51:43 +0000

    recipecard.jsx add to fav button, styling favouriteCard.jsx remove redundant button

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const RecipeCard = ({ recipe }) => {
+const RecipeCard = ({ recipe, addToFav }) => {
   const { idMeal, strMeal, strCategory, strMealThumb } = recipe;
   return (
     <div className="col-sm-3">
@@ -14,13 +14,24 @@ const RecipeCard = ({ recipe }) => {
         <div className="card-body">
           <span className="category">{strCategory}</span>
           <h3>{strMeal}</h3>
-          <a
-            href={"https://www.themealdb.com/meal/" + idMeal}
-            target="_blank"
-            rel="noreferrer"
-          >
-            Ingredients
-          </a>
+          <div className="d-flex justify-content-evenly align-items-center w-100">
+            <a
+              href={"https://www.themealdb.com/meal/" + idMeal}
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-secondary"
+            >
+              Ingredients
+            </a>
+            <button
+              className="btn btn-primary"
+              onClick={() => {
+                addToFav(recipe);
+              }}
+            >
+              Add To Favourites
+            </button>
+          </div>
         </div>
       </div>
     </div>
